fafcb0c3022c498192e3236ab05461aef28d456e
Mingxuan Li <74256506+Mil152@users.noreply.github.com>
2024-03-15 18:23:20 -0400

    updates

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'LayerLumos'
+copyright = '2024, Mingxuan Li'
+author = 'Mingxuan Li'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'nbsphinx',
+    'sphinx.ext.mathjax',  # If you have mathematical content
+    'sphinx.ext.autodoc',
+    # Other extensions here
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
